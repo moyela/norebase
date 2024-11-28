c8c8dabbf3f8f344d38df87cb9348ea65a1eb93d
Wemi Moyela <wemimoyela@gmail.com>
2024-11-28 23:50:24 +0100

    refactored bloated components

--- src/components/mobile_table_view.tsx
+++ src/components/mobile_table_view.tsx
@@ -1,40 +1,54 @@
-import { toggleLikeToken } from "../storage/tokenLocalStorage";
+import { toggleLikeToken } from "../storage/localStorage";
 
 export default function MobileTableView(tableData: any) {
     return (
-        <>
+        <> 
             {
-                tableData.map((token:any) => {
+                tableData.tableData.map((token:any, index:number) => {
             
                     return (
                         
-                        <li key={token.id} className='border-black border rounded-md flex sm:hidden  flex-col gap-2 bg-slate-100 p-1 mb-2'>  
-                            <div className="flex flex-row justify-between text-sm">
-                            <p className="hidden md:block basis-1/6">{token.rank}</p>
-                            <div className="basis-1/2 flex flex-col gap-3">
-                                <div>
-                                <p className="font-bold">🪙Token</p>
-                                <p><span className="font-bold">{token.rank}</span> {token.name}</p>
+                        <li key={token.nameid + ' mobile'} className={`flex sm:hidden flex-col p-1 mx-4 ${index % 2 == 0 ? 'bg-gray-200' : 'bg-white'}`}> 
+                            <div className="flex flex-row justify-between text-sm px-4 py-2">
+                                <div className="basis-1/2 flex flex-col gap-3">
+                                    <div>
+                                        <p className="font-bold font-mono text-xs">
+                                            🪙Token
+                                        </p>
+                                        <p>
+                                            <span className="font-bold">{token.rank}</span> {token.name}
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <p className="font-bold font-mono text-xs">
+                                            📶Symbol
+                                        </p>
+                                        <p>
+                                            (<span className="font-bold font-mono">{token.symbol}</span>)
+                                        </p>
+                                    </div>
                                 </div>
-                                <div>
-                                <p className="font-bold">📶Symbol</p>
-                                <p>({token.symbol})</p>
+                                <div className="basis-1/2 flex flex-col gap-3">
+                                    <div>
+                                        <p className="font-bold font-mono text-xs">
+                                            💵Price
+                                        </p>
+                                        <p>
+                                            <span className="font-bold">$</span><span className="font-bold text-red-600">{token.price_usd}</span></p>
+                                    </div>  
+                                    <div>
+                                        <p className="font-bold font-mono text-xs">
+                                            🔄️Total Supply
+                                        </p>
+                                        <p>{token.tsupply} <span className="font-bold font-mono">{token.symbol}</span></p>
+                                    </div>
                                 </div>
+                                <button className="text-[#66b179] font-extrabold" 
+                                onClick={() => toggleLikeToken(token.id, token.nameid, token.symbol, token.price_usd)}>
+                                    ❤️
+                                </button>
                             </div>
-                            <div className="basis-1/2 flex flex-col gap-3">
-                                <div>
-                                <p className="font-bold">💵Price</p>
-                                <p><span className="font-bold">$</span><span className="font-bold text-red-600">{token.price_usd}</span></p>
-                                </div>  
-                                <div>
-                                <p className="font-bold">🔄️Total Supply</p>
-                                <p>{token.tsupply} <span className="font-bold">{token.symbol}</span></p>
-                                </div>
-                            </div>
-                
-                            <button className="text-[#66b179] font-extrabold" onClick={() => toggleLikeToken(token.id, token.nameid, token.symbol, token.price_usd)}>❤️</button>
-                            </div>
-                        </li>                  
+                      </li>                
                     )
             })}   
         </>
--- src/routes/default_table.tsx
+++ src/routes/default_table.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import { toggleLikeToken } from "../storage/tokenLocalStorage";
 import { storeSessionStorageState, recallSessionStorageState } from "../storage/sessionStorage";
+import MobileTableView from "../components/mobile_table_view";
+import WebTableView from "../components/web_table_view";
 
 export default function Home() {
     const BASE_URL = "https://api.coinlore.net/api/"
@@ -61,7 +62,7 @@ export default function Home() {
       fetchTokens();
       let date = new Date();
       setFetchTime(`${date.toLocaleTimeString()} | ${date.toLocaleDateString()}`)
-    }, [start,page]);
+    }, [start, page]);
 
     if (loading === true) {return <div className="flex flex-row justify-around"><h1 className="pt-10 min-h-screen text-xl">Loading...</h1></div>}
 
@@ -85,66 +86,15 @@ export default function Home() {
           </div>
         </div>
 
+        {/* Actual data from useEffect */}
         <div>
           <ul>
-            {tokens.map((token:any, index:number) => {
-              return (
-                <>
-                  {/* // mobile-view -------------------------------------------------------------------------- */}
-                  <li key={token.nameid + ' mobile'} className={`flex sm:hidden flex-col p-1 mx-4 ${index % 2 == 0 ? 'bg-gray-200' : 'bg-white'}`}> 
-                    <div className="flex flex-row justify-between text-sm px-4 py-2">
-                      <div className="basis-1/2 flex flex-col gap-3">
-                        <div>
-                          <p className="font-bold font-mono text-xs">🪙Token</p>
-                          <p><span className="font-bold">{token.rank}</span> {token.name}</p>
-                        </div>
-                        <div>
-                          <p className="font-bold font-mono text-xs">📶Symbol</p>
-                          <p>(<span className="font-bold font-mono">{token.symbol}</span>)</p>
-                        </div>
-                      </div>
-                      <div className="basis-1/2 flex flex-col gap-3">
-                        <div>
-                          <p className="font-bold font-mono text-xs">💵Price</p>
-                          <p><span className="font-bold">$</span><span className="font-bold text-red-600">{token.price_usd}</span></p>
-                          </div>  
-                        <div>
-                          <p className="font-bold font-mono text-xs">🔄️Total Supply</p>
-                          <p>{token.tsupply} <span className="font-bold font-mono">{token.symbol}</span></p>
-                        </div>
-                      </div>
-                      <button className="text-[#66b179] font-extrabold" onClick={() => toggleLikeToken(token.id, token.nameid, token.symbol, token.price_usd)}>❤️</button>
-                    </div>
-                  </li>
-                  {/* ---------------------------------------------------------------------------------------- */}
-
-                  {/* // web-view -------------------------------------------------------------------------- */}
-                  <li key={token.nameid + ' web'} className={`hidden sm:flex  flex-col bg-slate-100 px-4 py-2 ${index % 2 == 0 ? 'bg-slate-200' : 'bg-white'}`}>  
-                
-                    <div className="flex flex-row justify-between text-sm">
-                      <p className="hidden sm:block basis-1/6">{token.rank}</p>
-                      <div className="basis-2/5">
-                        <p >{token.name}</p>
-                      </div>
-                      <div  className="basis-1/6">
-                        <p><span className="font-bold font-mono">{token.symbol}</span></p>
-                      </div>
-                      <div className="basis-1/4">
-                        <p><span className="font-bold">$</span><span className="font-bold text-red-600">{token.price_usd}</span></p>
-                      </div>
-                      <div className="basis-3/6">
-                        <p>{token.tsupply} <span className="font-bold font-mono">{token.symbol}</span></p>
-                      </div>
-                      <div className="relative pr-4">
-                        <button className="absolute hover:top-[-5px] text-[#66b179] font-extrabold hover:text-xl" onClick={() => toggleLikeToken(token.id, token.nameid, token.symbol, token.price_usd)}>❤️</button>
-                      </div>
-                    </div>
-                 
-                  </li>
-                  {/* ---------------------------------------------------------------------------------------- */}
-                </>
-              )
-            })}   
+            <div className="sm:hidden">
+              <MobileTableView tableData={tokens} />
+            </div>
+            <div className="hidden sm:block">
+              <WebTableView tableData={tokens} /> 
+            </div>
           </ul>
         </div>
         {/* footer of the table */}
